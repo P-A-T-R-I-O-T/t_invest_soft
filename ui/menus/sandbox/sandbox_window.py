1c5b334f12fb8994bb234ec6e129331122615c1f
Konstantin Kolesov <P-A-T-R-I-O-T-kolesow@yandex.ru>
2026-03-16 19:44:19 +0300

    переделал окно настройки песочницы, теперь не нужно открывать дополнительное окно в момент создания счёта, всё будет и создание и радактирование в одном окне

--- ui/menus/sandbox/sandbox_window.py
+++ ui/menus/sandbox/sandbox_window.py
@@ -10,8 +10,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QLabel, QListWidget,
     QListWidgetItem, QPushButton, QRadioButton, QSizePolicy,
-    QTextEdit, QDialog)
-from ui.menus.sandbox.create_account import CreateAccountDialog
+    QTextEdit, QDialog, QLineEdit)
+#from ui.menus.sandbox.create_account import CreateAccountDialog
 
 
 class SandboxSettingsWindow(QDialog):  # Наследуем от QDialog для отдельного окна
@@ -54,45 +54,37 @@ class SandboxSettingsWindow(QDialog):  # Наследуем от QDialog для 
         self.label.setGeometry(QRect(30, 100, 41, 20))
 
         self.listWidget = QListWidget(self)
-        self.listWidget.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
-        self.listWidget.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
         self.listWidget.setObjectName(u"listWidget")
         self.listWidget.setGeometry(QRect(30, 130, 200, 150))
-
-        self.pushButton_3 = QPushButton(self)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(30, 324, 79, 24))
 
         self.pushButton_2 = QPushButton(self)
         self.pushButton_2.setObjectName(u"pushButton_2")
         self.pushButton_2.setGeometry(QRect(30, 290, 80, 24))
-        self.pushButton_2.clicked.connect(self.open_create_account_dialog)
-
 
         self.label_2 = QLabel(self)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(260, 130, 140, 20))
+        self.label_2.setGeometry(QRect(260, 160, 140, 20))
 
         self.radioButton_2 = QRadioButton(self)
         self.radioButton_2.setObjectName(u"radioButton_2")
-        self.radioButton_2.setGeometry(QRect(260, 160, 45, 20))
+        self.radioButton_2.setGeometry(QRect(260, 190, 45, 20))
 
         self.radioButton_3 = QRadioButton(self)
         self.radioButton_3.setObjectName(u"radioButton_3")
-        self.radioButton_3.setGeometry(QRect(260, 180, 45, 20))
+        self.radioButton_3.setGeometry(QRect(260, 212, 45, 20))
 
         self.radioButton = QRadioButton(self)
         self.radioButton.setObjectName(u"radioButton")
-        self.radioButton.setGeometry(QRect(260, 200, 45, 20))
+        self.radioButton.setGeometry(QRect(260, 234, 45, 20))
 
         self.comboBox = QComboBox(self)
         self.comboBox.setObjectName(u"comboBox")
-        self.comboBox.setGeometry(QRect(430, 160, 200, 22))
+        self.comboBox.setGeometry(QRect(430, 232, 200, 22))
         self.comboBox.setEditable(True)
 
         self.label_3 = QLabel(self)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(430, 130, 70, 20))
+        self.label_3.setGeometry(QRect(430, 212, 70, 20))
 
         self.pushButton_4 = QPushButton(self)
         self.pushButton_4.setObjectName(u"pushButton_4")
@@ -102,27 +94,53 @@ class SandboxSettingsWindow(QDialog):  # Наследуем от QDialog для 
         self.pushButton_5.setObjectName(u"pushButton_5")
         self.pushButton_5.setGeometry(QRect(260, 324, 150, 24))
 
+        self.radioButton_4 = QRadioButton(self)
+        self.radioButton_4.setObjectName(u"radioButton_4")
+        self.radioButton_4.setGeometry(QRect(260, 130, 81, 20))
+
+        self.radioButton_5 = QRadioButton(self)
+        self.radioButton_5.setObjectName(u"radioButton_5")
+        self.radioButton_5.setGeometry(QRect(430, 130, 71, 20))
+
+        self.lineEdit = QLineEdit(self)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(430, 180, 200, 22))
+
+        self.label_7 = QLabel(self)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setGeometry(QRect(430, 160, 90, 20))
+
+        self.pushButton_3 = QPushButton(self)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(385, 274, 90, 24))
+
         self.retranslateUi()
         QMetaObject.connectSlotsByName(self)
 
     def retranslateUi(self):
-        self.setWindowTitle(QCoreApplication.translate("Form", u"Настройки песочницы", None))
+        self.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.label_4.setText(QCoreApplication.translate("Form", u"Баланс счёта", None))
         self.label_5.setText(QCoreApplication.translate("Form", u"Отображение баланса", None))
         self.label_6.setText(QCoreApplication.translate("Form", u"Валюта", None))
         self.textEdit.setPlaceholderText(QCoreApplication.translate("Form", u"Отображение ТОКЕНА", None))
         self.pushButton.setText(QCoreApplication.translate("Form", u"Показать токен", None))
         self.label.setText(QCoreApplication.translate("Form", u"Счета:", None))
-        self.pushButton_3.setText(QCoreApplication.translate("Form", u"Удалить счёт", None))
-        self.pushButton_2.setText(QCoreApplication.translate("Form", u"Создать счёт", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Form", u"Удалить счёт", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"Изменить валюту счёта", None))
         self.radioButton_2.setText(QCoreApplication.translate("Form", u"ERU", None))
         self.radioButton_3.setText(QCoreApplication.translate("Form", u"RUB", None))
         self.radioButton.setText(QCoreApplication.translate("Form", u"USD", None))
         self.comboBox.setCurrentText(QCoreApplication.translate("Form", u"сумма счёта", None))
+        self.comboBox.setPlaceholderText("")
         self.label_3.setText(QCoreApplication.translate("Form", u"Пополнить", None))
         self.pushButton_4.setText(QCoreApplication.translate("Form", u"Применить", None))
         self.pushButton_5.setText(QCoreApplication.translate("Form", u"Отменить все действия", None))
+        self.radioButton_4.setText(QCoreApplication.translate("Form", u"Изменить", None))
+        self.radioButton_5.setText(QCoreApplication.translate("Form", u"Создать", None))
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("Form", u"Введите имя счёта", None))
+        self.label_7.setText(QCoreApplication.translate("Form", u"Название счёта", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Form", u"Изменить счёт", None))
+
 
     def update_accounts_list(self):
         """Обновляет список счетов в listWidget."""
